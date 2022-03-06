71bc5bdbf9e86fd127c04be1bfb2d000c0301861
murat06aksoy <101042855+murat06aksoy@users.noreply.github.com>
2022-03-06 15:58:16 +0300

    Add files via upload

---
+++ ders1/27022022-1.py
@@ -0,0 +1,12 @@
+""""
+burası
+açıklama
+satırıdır
+"""
+print("merhaba python")#burası tek satırlık açıklama satırıdır
+print("merhaba python")#ctrl+ d satır çoklar,ctrl+x,ctrl+y satır siler
+print("merhaba python")
+print("benim adım murat","BTK AKADEMİ",123)
+print("benim adım murat"+"BTK AKADEMİ",123)
+print(123+456)
+print("123"+"456")
---
+++ ders1/27022022-2.py
@@ -0,0 +1,14 @@
+isim=input("Adınız:")
+print("Hoşgeldin",isim)
+sayi1=int(input("1. sayiyi giriniz:"))#inputtan girilen değerler string değerindedir!
+sayi2=int(input("2. sayiyi giriniz:"))
+
+"""sonuc=int(sayi1)+int(sayi2)"""
+"""(sonuc:",sonuc)"""
+print("sayılarınızın toplamı:",sayi1+sayi2)
+print("sayılarnızın farkı:",sayi1-sayi2)
+print("sayılarnızın çarpımı:",sayi1*sayi2)
+print("sayılarnızın bölümü:",sayi1//sayi2)
+print("sayılarnızın tam bölümü:",sayi1//sayi2)
+print(sayi1,"üzeri",str(sayi2)+":",sayi1**sayi2)
+print(sayi1,"sayısının", sayi2,"sayısına bölümünden kalan:",sayi1%sayi2)
